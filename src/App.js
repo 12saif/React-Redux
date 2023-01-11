7e495f8e7486e08712bc1e659114b58a0dbc6d7b
md saif <mohdsaif9170613062@gmail.com>
2023-01-11 14:12:45 +0530

    Update React-Redux

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { useSelector, useDispatch } from 'react-redux';
+import { incNumber, decNumber } from './action/index'
 
-function App() {
+const App = () => {
+  const myState = useSelector((state) => state.changeTheNumber );
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <h1>Lorem
-        
-      </h1>
-    </div>
-  );
+    <>
+      <div className="container">
+        <h1>Increment/Decrement Counter</h1>
+        <h4>Using React and Redux</h4>
+
+        <div className="quantity">
+          <a className="quantity__minus" tital="Decrement"
+          onClick={()=> dispatch(decNumber())}>
+            <span> - </span>
+          </a>
+          <input
+            name="quantity"
+            type="text"
+            className="quantity__input"
+            value={myState}
+          />
+          <a className="quantity__plus" tital="Increment" 
+          onClick={()=> dispatch(incNumber())}>
+            <span> + </span>
+          </a>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/action/index.jsx
@@ -0,0 +1,10 @@
+export const incNumber = () => {
+  return {
+    type: 'INCREMENT',
+  }
+}
+export const decNumber = () => {
+  return {
+    type: 'DECREMENT',
+  }
+}
